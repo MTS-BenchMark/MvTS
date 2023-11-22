b7a4f720c50b53771b08c3ff04b551668399e0d6
lwm412 <87411418+lwm412@users.noreply.github.com>
2023-11-22 15:07:55 +0800

    Update pipeline.py

--- mvts/pipeline/pipeline.py
+++ mvts/pipeline/pipeline.py
@@ -1,26 +1,13 @@
 import os
-import json
-import torch
 
 from mvts.config import ConfigParser
 from mvts.data import get_dataset
 from mvts.utils import get_executor, get_model, get_logger, ensure_dir
 
 
-# mts, LstNet, traffic, 
 def run_model(task=None, model_name=None, dataset_name= None, config_file=None,
               saved_model=True, train=True, other_args=None):
-    """
-    Args:
-        task(str): task name
-        model_name(str): model name
-        dataset_name(str): dataset name
-        config_file(str): config filename used to modify the pipeline's
-            settings. the config file should be json.
-        saved_model(bool): whether to save the model
-        train(bool): whether to train the model
-        other_args(dict): the rest parameter args, which will be pass to the Config
-    """
+    
     # load config
     config = ConfigParser(task, model_name, dataset_name,
                           config_file, saved_model, train, other_args)
